1a9362ad6909689e7afe9e0bb176f540fbdab113
JeiWolfei <jvampy22@gmail.com>
2018-12-04 17:27:54 -0800

    hard coded data for islands.js

---
+++ server/data/islands.js
@@ -0,0 +1,37 @@
+[
+  {
+    "name": "St. Lucia",
+    "loc": "Carribean",
+    "id": ""
+  },
+  {
+    "name": "Fiji",
+    "loc": "South Pacific",
+    "id": ""
+  },
+  {
+    "name": "Maui",
+    "loc": "Hawaiian Islands",
+    "id": ""
+  },
+  {
+    "name": "Maldives",
+    "loc": "Indian Ocean",
+    "id": ""
+  },
+  {
+    "name": "Santorini, Greece",
+    "loc": "Aegean Sea",
+    "id": ""
+  },
+  {
+    "name": "Bora-Bora",
+    "loc": "South Pacific",
+    "id": ""
+  },
+  {
+    "name": "Palawan",
+    "loc": "Philippines",
+    "id": ""
+  }
+]
