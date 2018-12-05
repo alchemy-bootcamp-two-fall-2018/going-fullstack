1d8f948dcd56262b881e8161cf5d6d56d8afd44b
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-12-04 16:46:55 -0800

    racers rendering on page

--- server/server.js
+++ server/server.js
@@ -1,3 +1,27 @@
-// const express = require('express');
-// const app = express();
-// const fs = require('fs');
+const express = require('express');
+const app = express();
+// const shortid = require('shortid');
+
+const fs = require('fs');
+
+function readData() {
+  const data = fs.readFileSync('./data/racers.json', 'utf8');
+  return JSON.parse(data);
+}
+
+app.use(express.json());
+
+app.get('/api/racers', (req, res) => {
+  const racers = readData();
+  res.json(racers);
+});
+
+// app.post('/api/racers', (req, res))
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('server app started on port', PORT);
+});
+
+
