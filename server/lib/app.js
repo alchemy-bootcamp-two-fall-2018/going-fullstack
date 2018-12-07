74e09683181d1e18d309ae1f2f81b06b4b331d54
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-07 13:46:51 -0800

    created new file folders for server folder with lib folder and routes folder, added app.js and routes files, working with new routes in postman right now

---
+++ server/lib/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const animals = require('./routes/animals');
+const size = require('./routes/sizes');
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use('/api/sizes', size);
+app.use('/api/animals', animals);
+
+module.exports = app;
+
---
+++ server/lib/routes/animals.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const client = require('../../db-client');
+const Router = express.Router;
+const router = Router(); //eslint-disable-line new-cap
+
+
+
+router.get('/', (req, res) => {
+  client.query(`
+    SELECT 
+      animals.id, 
+      animals.name as name, 
+      animals.weight as weight,
+      animals.mammal as mammal,
+      animals.image as image,
+      size.id as "sizeId",
+      size.name as size
+    FROM animals
+    JOIN size
+    ON animals.size_id = size.id
+    ORDER BY name ASC;
+  `)
+    .then(result => {
+      res.json(result.rows);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  client.query(`
+  SELECT 
+    animals.id, 
+    animals.name as name, 
+    animals.weight as weight,
+    animals.mammal as mammal,
+    animals.image as image,
+    size.id as "sizeId",
+    size.name as size
+  FROM animals
+  JOIN size
+  ON animals.size_id = size.id
+  WHERE animals.id = $1;
+    
+  `,
+  [req.params.id])
+    .then(result => {
+      res.json(result.rows[0]);
+    });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+
+  client.query(`
+    INSERT INTO animals (name, weight, mammal, size_id, image)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING id;
+  `,
+  [body.name, body.weight, body.mammal, body.sizeId, body.image])
+    .then(result => {
+      const id = result.rows[0].id;
+
+      return client.query(` 
+      SELECT
+        animals.id,
+        animals.name as name,
+        animals.weight as weight,
+        animals.mammal as mammal,
+        size.id as "sizeId",
+        size.name as size
+      FROM animals
+      JOIN size
+      ON animals.size_id = size.id
+      WHERE animals.id = $1;
+      `,
+      [id]);
+    })
+    .then(result => {
+      res.json(result.rows[0]);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  client.query(`
+    DELETE FROM animals WHERE id = $1;
+  `,
+  [req.params.id])
+    .then(result => {
+      res.json({ removed:result.rowCount === 1 });
+    });
+});
+
+module.exports = router;
