26cb1a363acb9ac4c869090a798117d70d8fd738
Marcy Silverman <marcysilve225@gmail.com>
2018-12-07 14:27:56 -0800

    set up modal

---
+++ app/src/components/dogSizes/DogDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <span v-i="size">
+    {{size.name}}
+  </span>
+</template>
+
+<script>
+import api from '../../services/api'; 
+
+export default {
+  props: {
+    sizeId: Number
+  }, 
+  data() {
+    return {
+      size: null
+    };
+  }, 
+  created() {
+    api.getSizes()
+      .then(sizes => {
+        this.size = sizes.find(size => size.id === this.sizeId); 
+      });
+  }
+}
+</script>
+
+<style>
+
+</style>
