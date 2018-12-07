0a12e272c963a270b7dfabf3a8c13ca99ba9bdda
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-06 16:21:06 -0800

    adding working with guitar join

--- server/server.js
+++ server/server.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const pg = require('pg');
+const client = require('./db-client');
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-const Client = pg.Client;
-const dbUrl = 'postgres://localhost:5432/guitarists';
-const client = new Client(dbUrl);
-client.connect();
-
-
-
-app.get('/api/guitarists', (req, res) => {
+app.get('/api/guitars', (req, res) => {
   client.query(`
-    SELECT id, name FROM guitarists;
+    SELECT id, brand, model
+    FROM guitar
+    ORDER BY brand;
   `)
     .then(result => {
       res.json(result.rows);
     });
+});
 
+
+app.get('/api/guitarists', (req, res) => {
+  client.query(`
+    SELECT 
+      guitarists.id, 
+      guitarists.name as name,
+      guitarists.yob as yob,
+      guitar.id as "guitarId",
+      guitarists.type as type
+    FROM guitarists
+    JOIN guitar
+    ON guitarists.guitar_id = guitar.id
+    ORDER BY name ASC;
+  `)
+    .then(result => {
+      res.json(result.rows);
+    });
 });
 
 app.get('/api/guitarists/:id', (req, res) => {
@@ -37,11 +50,29 @@ app.post('/api/guitarists', (req, res) => {
   const body = req.body;
 
   client.query(`
-    INSERT INTO guitarists (name, type, yob, alive)
+    INSERT INTO guitarists (name, guitar_id, yob, type)
     VALUES($1, $2, $3, $4)
-    RETURNING id, name, type, yob, alive;
+    RETURNING id;
   `,
-  [body.name, body.type, body.yob, body.alive])
+  [body.name, body.guitarId, body.yob, body.type])
+    .then(result => {
+      const id = result.rows[0].id;
+      
+      return client.query(`
+        SELECT 
+          guitarists.id, 
+          guitarists.name as name,
+          guitarists.yob as "yob",
+          guitar.id as "guitarId",  
+          guitar.model as guitar,
+          guitarists.type
+        FROM guitarists
+        JOIN guitar
+        ON guitarists.guitar_id = guitar.id 
+        WHERE guitarists.id = $1;
+      `,
+      [id]);
+    })
     .then(result => {
       res.json(result.rows[0]);
     });
