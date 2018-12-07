f261f008ee517f881b739448d2a8e0ccd604b1a0
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-12-06 16:23:25 -0800

    added a second table to the create table/ drop table/load seed data scripts

--- server/scripts/load-seed-data.js
+++ server/scripts/load-seed-data.js
@@ -17,7 +17,13 @@ Promise.all(
                 return client.query (`
                 INSERT INTO grapplers (name, age, champ, pref_id)
                 SELECT
+
                     $1 as name, 
+
+
+
+
+                    
                     $2 as age, 
                     $3 as champ,
                     id as pref_id
--- server/scripts/pref.js
+++ server/scripts/pref.js
@@ -2,6 +2,4 @@ module.exports = [
     { name: 'RASHGUARD AND FANNYPACK GANG', shortName: 'NO-GI' }, 
 
     { name: 'PAJAMA AND SQUID GAURD GANG', shortName: 'GI' }
-
-    
 ];
