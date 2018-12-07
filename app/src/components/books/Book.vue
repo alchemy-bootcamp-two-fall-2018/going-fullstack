ebdec8e63e3bb13040c6f1a9e40cc1b7ee9d953b
Cheri Portman <cheriportman@hotmail.com>
2018-12-06 21:01:16 -0800

    time out for day

---
+++ app/src/components/books/Book.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <RouterLink :to="`/books/${book.id}`">
+      {{book.title}} </RouterLink>
+    by {{book.author}}.<template>
+  </li>
+</template>
+    
+<script>
+export default {
+  props: {
+    book: null
+  }
+};
+</script>
+
+<style>
+
+</style>
+    
