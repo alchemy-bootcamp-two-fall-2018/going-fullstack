1204d234b04987a69bd9d4c995943255004975fd
Cheri Portman <cheriportman@hotmail.com>
2018-12-07 17:54:57 -0800

    functioning!

--- server/server.js
+++ server/server.js
@@ -57,24 +57,24 @@ app.post('/api/books', (req, res) => {
   const body = req.body;
 
   client.query(`
-    INSERT INTO book (title, author, pages, good)
+    INSERT INTO books (title, author, pages, genre_id)
     VALUES($1, $2, $3, $4)
-    RETURNING id, title, author, pages, good;
+    RETURNING id, title, author, pages, genre_id;
   `,
-  [body.title, body.author, body.pages, body.good])
+  [body.title, body.author, body.pages, body.genreId])
     .then(result => {
       const id = result.rows[0].id;
 
       return client.query(`
         SELECT
-          book.id,
-          book.title as title,
+          books.id,
+          books.title as title,
           genre.id as "genreId",
           genre.genre as genre
-        FROM book
+        FROM books
         JOIN genre
-        ON book.genre_id = genre.id
-        WHERE book.id = $1;
+        ON books.genre_id = genre.id
+        WHERE books.id = $1;
       `,
       [id]);
     })
