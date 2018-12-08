b22f117fdaed84234451fa57f177b21afbaae31b
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-12-08 10:11:32 -0800

    ordered list working

--- app/src/components/RacerList.vue
+++ app/src/components/RacerList.vue
@@ -1,7 +1,7 @@
 <template>
     <ul v-if="racers">
         <li
-            v-for="racer in racers"
+            v-for="racer in sortedRacers"
             :key="racer.id">
                 <RouterLink :to="`/racers/${racer.id}`"> 
                     {{racer.name}}
@@ -15,6 +15,15 @@
 export default {
     props: {
         racers: null
+    },
+    computed: {
+        sortedRacers() {
+            return this.racers.slice().sort((a, b) => {
+                if(a.name > b.name) return 1;
+                if(a.name < b.name) return -1;
+                return 0;
+            });
+        }
     }
 };
 </script>
