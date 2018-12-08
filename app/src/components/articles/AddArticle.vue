c211b4200e577108d6e1ed09347a83f42ee74aaf
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-08 14:32:49 -0800

    fixing wiring for AddArticle

--- app/src/components/articles/AddArticle.vue
+++ app/src/components/articles/AddArticle.vue
@@ -1,33 +1,67 @@
 <template>
   <form @submit.prevent="handleSubmit">
+    <p>
+      <label>Title:</label>
     <input v-model="article.title" require>
+    </p>
+
+    <p>
+      <label>Author:</label>
+      <textarea v-model="article.author"></textarea>
+    </p>
+
+    <p>
+      <label>Category:</label>
+      <textarea v-model="article.category"></textarea>
+    </p>
+
     <button>Add</button>
+    
   </form>
 </template>
 
 <script>
+import api from '../../services/api.js';
+
+function initArticle() {
+  return {
+    title: '',
+    author: '',
+    category: 'us',
+    views: ''
+  };
+}
+
 export default {
   props: {
     onAdd: Function
   },
   data() {
     return {
-      article: {
-        title: ''
-      }
-    };
+      article: initArticle(),
+      category: null
+      };
+    },
+  created() {
+    this.category = api.getCategories();
   },
   methods: {
     handleSubmit() {
       this.onAdd(this.article)
         .then(() => {
-          this.article = { title: '' };
+          this.article = initArticle();
         });
     }
   }
 };
 </script>
 
-<style>
-
+<style scoped>
+label {
+  display: block;
+}
+input, select {
+  width: 150px;
+  font-size: 1.05em;
+}
 </style>
