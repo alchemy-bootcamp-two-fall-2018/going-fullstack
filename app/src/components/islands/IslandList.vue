5cf3501fbfa2ecb9587e9f1353eb2089a27275a2
JeiWolfei <jvampy22@gmail.com>
2018-12-10 12:36:21 -0800

    editing css

--- app/src/components/islands/IslandList.vue
+++ app/src/components/islands/IslandList.vue
@@ -1,7 +1,7 @@
 <template>
   <ul v-if="islands">
     <li v-for="island in islands" :key="island.id">
-      <RouterLink :to="`/islands/${island.id}`"><h3 class="info">{{islands.name}}</h3></RouterLink>
+      <RouterLink :to="`/islands/${island.id}`"><h3 class="info">{{island.name}}</h3></RouterLink>
       <img v-bind:src="island.image"/>
     </li>
   </ul>
@@ -16,14 +16,42 @@ export default {
 </script>
 
 <style scoped>
-li {
-  list-style: none;
-  position: relative;
-  width: 30%;
-  height: 30%;
-  border: 1px rgb(170, 117, 20)
-}
-li img {
+a {
+    color: rgb(214, 115, 22);
+    text: black;
+  }
+  h3:hover {
+    background: rgb(231, 235, 12);
+    color: rgb(58, 190, 241);
+  }
+  li {
+    list-style: none;
+    position: relative;
+    width: 350px;
+    height: 350px;
+    border: 2px solid black;
+    text-align: center;
+    margin: 20px;
+  }
+  
+  h3 {
+    text-align: center;
+    z-index: 1;
+    margin: 0px;
+    font-size: 1.4em;
+  }
+  .info {
+    background: rgba(255, 255, 255, 0.8);
+    margin-top: 0px;
+    padding: 10px;
+    border-bottom: 2px solid black;
+  }
+  ul {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  li img {
     position: absolute;
     top: 0;
     left: 0;
@@ -31,15 +59,5 @@ li img {
     height: 100%;
     z-index: -1;
     object-fit: cover;
-}
-.info {
-  background: rgb(192, 209, 240);
-  padding: 10px;
-}
-ul {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
+  }
 </style>
