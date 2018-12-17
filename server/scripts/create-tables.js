4de2cf5bb4ab26a4626beed70435a2105361e7d7
Ivan <i.poblete915@gmail.com>
2018-12-17 12:52:44 -0800

    fixed syntax error

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -1,7 +1,7 @@
 const client = require('../db-client');
 
 client.query(`
-  CREATE TABLE IF NOT EXISTS group (
+  CREATE TABLE IF NOT EXISTS groupie (
     id SERIAL PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     short_name VARCHAR(8) NOT NULL
@@ -10,7 +10,7 @@ client.query(`
   CREATE TABLE IF NOT EXISTS hero (
     id SERIAL PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
-    group_id INTEGER NOT NULL REFERENCES group(id),
+    groupie_id INTEGER NOT NULL REFERENCES groupie(id),
     age INTEGER,
     ability VARCHAR(50)
   );
