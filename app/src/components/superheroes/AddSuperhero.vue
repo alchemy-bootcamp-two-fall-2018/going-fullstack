dba3ed62b6c4a3a9248f27c4a195fa76ed7bd4f6
Ivan <i.poblete915@gmail.com>
2018-12-17 22:00:35 -0800

    add superhero working again

--- app/src/components/superheroes/AddSuperhero.vue
+++ app/src/components/superheroes/AddSuperhero.vue
@@ -12,18 +12,34 @@
       <label>Ability:</label>
       <input v-model="superhero.ability">
     </p>
+    <p>
+      <label>Gang:</label>
+      <select v-if="gangs" 
+        v-model="superhero.gangId" 
+        required
+      >
+        <option value="-1" disabled>Select a Gang</option>
+        <option v-for="gang in gangs"
+          :key="gang.id"
+          :value="gang.id"
+        >
+          {{gang.name}} ({{gang.shortName}})
+        </option>
+      </select>
+    </p>
     <button>Add</button>
   </form>
 </template>
 
 <script>
-// import api from '../../services/api';
+import api from '../../services/api';
 
 function initSuperhero() {
   return {
     name: '',
     age: '',
-    ability: ''
+    ability: '',
+    gangId: -1,
   };
 }
 
@@ -33,8 +49,15 @@ export default {
   },
   data() {
     return {
-      superhero: initSuperhero()
+      superhero: initSuperhero(),
+      gangs: null
     };
+  },
+  created() {
+    api.getGangs() 
+      .then(gangs => {
+        this.gangs = gangs;
+      });
   },
   methods: {
     handleSubmit() {
